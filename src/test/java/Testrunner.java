86f29f645bd5a96ca8c85f481a598f184e441c45
khalid01427 <42937099+khalid01427@users.noreply.github.com>
2024-12-22 21:29:24 +0530

    Update Testrunner.java

--- src/test/java/Testrunner.java
+++ src/test/java/Testrunner.java
@@ -7,7 +7,7 @@ public class Testrunner {
     public void abc()
 {
 
-    System.out.println("Hello Mr. Md Khalid Anwar");
+    System.out.println("Hello world");
     System.out.println("This will be executed using jenkins");
     System.out.println("This will be additional line");
     System.out.println("This will be additional line2 end of line...");
